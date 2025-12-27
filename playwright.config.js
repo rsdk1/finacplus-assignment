421aa015ddacfff93747c9011dbf8e3c535af155
rsdk1 <rs.dk.00001@gmail.com>
2025-12-27 22:08:00 +0530

    adding config file

---
+++ playwright.config.js
@@ -0,0 +1,49 @@
+// @ts-check
+import { defineConfig, devices } from '@playwright/test';
+
+import dotenv from 'dotenv';
+import path from 'path';
+dotenv.config({ path: path.resolve(__dirname, '.env') });
+
+export default defineConfig({
+  testDir: './tests',
+  /* Run tests in files in parallel */
+  fullyParallel: false,
+  /* Fail the build on CI if you accidentally left test.only in the source code. */
+  forbidOnly: !!process.env.CI,
+  /* Retry on CI and local */
+  retries: 0,
+  workers: 1,
+  /* Reporter to use. See https://playwright.dev/docs/test-reporters */
+  reporter: [
+    ['html'],
+    ['playwright-ctrf-json-reporter',{}]
+  ],
+  /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
+  use: {
+    /* Base URL to use in actions like `await page.goto('')`. */
+    baseURL: process.env.UI_BASE_URL,
+    trace: 'on',
+    screenshot: 'on',
+    video: 'on'
+  },
+
+  /* Configure projects for major browsers */
+  projects: [
+    {
+      name: 'Microsoft Edge',
+      use: { ...devices['Desktop Edge'], channel: 'msedge' },
+    },
+    {
+      name: 'Google Chrome',
+      use: { ...devices['Desktop Chrome'], channel: 'chrome' },
+    },
+    {
+      name: 'Firefox',
+      use: { ...devices['Desktop Firefox'] },
+    }
+    
+  ],
+
+});
+
