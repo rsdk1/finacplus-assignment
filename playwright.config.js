c803e9d58180e27bc4fc994abac3387ca40645c7
rsdk1 <rs.dk.00001@gmail.com>
2025-12-27 23:35:57 +0530

    changes for parallel execution

--- playwright.config.js
+++ playwright.config.js
@@ -13,7 +13,7 @@ export default defineConfig({
   forbidOnly: !!process.env.CI,
   /* Retry on CI and local */
   retries: 0,
-  workers: 1,
+  workers: 2,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: [
     ['html'],
